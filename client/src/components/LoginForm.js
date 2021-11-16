a30531515813b2f3f1b0868c87486f69190bd433
Justin Bedford <jbedford94@gmail.com>
2021-11-15 20:42:18 -0500

    forms for login

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -17,6 +17,45 @@ const LoginForm = () => {
       setUserFormData({ ...userFormData, [name]: value });
     };
 
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+    
+    // check if form has everything (as per react-bootstrap docs)
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+          Something went wrong with your login credentials!
+        </Alert>
+        <Form.Group>
+          <Form.Label htmlFor='email'>Email</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Your email'
+            name='email'
+            onChange={handleInputChange}
+            value={userFormData.email}
+            required
+          />
+          <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label htmlFor='password'>Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Your password'
+            name='password'
+            onChange={handleInputChange}
+            value={userFormData.password}
+            required
+          />
+          <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
+        </Form.Group>
 
 
 
@@ -24,9 +63,8 @@ const LoginForm = () => {
 
 
 
-
-
-
-
+        </>
+        );
+      };
 
 export default LoginForm;
