c29afdc28ec7625a39bee588e081fc5a16c024d2
caroline <cluce059@gmail.com>
2021-11-16 17:14:22 -0500

    deps

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,5 @@
 //dashboard displaying possible workouts to browze  
 //option to select only if logged in
-<<<<<<< HEAD
-import React from "react";
-import Auth from '../utils/auth';
-import { useMutation } from '@apollo/react-hooks';
-import {  Container, Card, CardColumns } from 'react-bootstrap';
-
-
-const Home = () => {
-    //create state to hold exercise api data
-    //create state to hold saved exercises
-
-=======
 import React, { useEffect } from "react";
 import {  Container, Card, CardColumns } from 'react-bootstrap';
 
@@ -27,23 +15,10 @@ const Home = () => {
 
     const [savedCircuitIds, setSavedCircuitIds] = useState(getSavedCircuitId());
     const [saveCircuit] = useMutation(SAVE_CIRCUIT);
->>>>>>> develop
     // set up useEffect hook to save `savedExercises` 
     //list to localStorage on component unmount too keep pwa functionality
 //possible to set unmount to after 2 weeks of no use?
     //method to display api data 
-<<<<<<< HEAD
-    const exerciseList = exercises.data.map((exercise) => (
-        <div className="col-xs-12 col-md-4 col" key={exercise.id}>
-          <div className="box">
-            <PetSection exercise={exercise} />
-          </div>
-        </div>
-      ));
-
-            //should this take acircuitId from a curcuit thats been selected?
-      const handleSaveExercise = async(exerciseId )
-=======
 
     useEffect(() => {
         return () => saveCircuitId(savedCircuitIds);
@@ -83,7 +58,6 @@ const Home = () => {
           }
       };
 
->>>>>>> develop
       return (
           <>
         <Container>
@@ -95,30 +69,13 @@ const Home = () => {
                     <Card key = {circuit.curcuitId}>
                         <Card.Body>
                             <Card.Title>{circuit.name}</Card.Title>
-<<<<<<< HEAD
-                            <p className="small">Reps: {circuit.reps}</p>
-=======
->>>>>>> develop
                         </Card.Body>
                         <Card.Text>
                             {circuit.exercises.map((exercise) => {
                                 return(
-<<<<<<< HEAD
-                                    <p>{exercise.name}</p>
-                                );
-                                <Button
-                                disabled={savedCircuitIds?.some((savedCircuitId) => savedCircuitId === circuit.circuitId)}
-                                className='btn-block btn-info'
-                                onClick={() => handleSaveCircuit(circuit.circuitId)}>
-                                {savedCircuitIds?.some((savedCircuitId) => savedBookId === book.bookId)
-                                  ? 'This book has already been saved!'
-                                  : 'Save this Book!'}
-                              </Button>
-=======
                                     <p>{exercise.name} x {exercise.reps}</p>
                                 );
                                
->>>>>>> develop
                             })}
                         </Card.Text>
                     </Card>
@@ -127,9 +84,6 @@ const Home = () => {
         </CardColumns>
         </>
       )
-<<<<<<< HEAD
-};
-=======
 };
 
 export default Home;
@@ -144,4 +98,3 @@ export default Home;
                                   ? 'This book has already been saved!'
                                   : 'Save this Book!'}
                               </Button> */
->>>>>>> develop
