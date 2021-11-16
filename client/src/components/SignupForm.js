c69a85503208944b500caec79b5e31e9f943e3ca
Justin Bedford <jbedford94@gmail.com>
2021-11-15 20:25:13 -0500

    sign up form tenative

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -6,10 +6,55 @@ import { useMutation } from '@apollo/client';
 
 
 const SignupForm = () => {
-  // set initial form state
-  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
-  // set state for form validation
-  const [validated] = useState(false);
+// set initial form state
+    const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+// set state for form validation
+    const [validated] = useState(false);
+
+// set state for alert
+    const [showAlert, setShowAlert] = useState(false);
+    const [addUser, { error }] = useMutation(ADD_USER);
+  
+    const handleInputChange = (event) => {
+    const { name, value } = event.target;
+      setUserFormData({ ...userFormData, [name]: value });
+    };
+  
+    const handleFormSubmit = async (event) => {
+      event.preventDefault();
+  
+// check if form has everything (as per react-bootstrap docs)
+    const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+  
+      try {
+        const { data } = await addUser({
+          variables: { ...userFormData }
+        })
+  
+        if (error) {
+          throw new Error('something is wrong!');
+        }
+  
+        
+       
+        Auth.login(data.addUser.token);
+
+        
+      } catch (err) {
+        console.error(err);
+        setShowAlert(true);
+      }
+  
+      setUserFormData({
+        username: '',
+        email: '',
+        password: '',
+      });
+    };
 
 
 return (
