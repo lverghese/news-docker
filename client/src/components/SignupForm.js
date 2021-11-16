76fb1c5eee7147b23825edb572d000489c160b15
Justin Bedford <jbedford94@gmail.com>
2021-11-15 20:13:38 -0500

    signup return

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,20 +1,41 @@
 import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Alert } from 'react-bootstrap';
 import AuthService from '../utils/auth';
 import { ADD_USER } from '../utils/mutations';
 import { useMutation } from '@apollo/client';
 
 
 const SignupForm = () => {
-
+  // set initial form state
+  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+  // set state for form validation
+  const [validated] = useState(false);
 
 
 return (
     <>
+      {/* This is needed for the validation functionality above */}
+      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+        {/* show alert if server response is bad */}
+        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+          You've bamboozled  your signup dummy!
+        </Alert>
 
 
-
-</>
+        <Form.Group>
+          <Form.Label htmlFor='username'>Username</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Your username'
+            name='username'
+            onChange={handleInputChange}
+            value={userFormData.username}
+            required
+          />
+            <Form.Control.Feedback type='invalid'>Input a Username or else!</Form.Control.Feedback>
+        </Form.Group>
+        </Form>
+    </>
   );
 };
 
