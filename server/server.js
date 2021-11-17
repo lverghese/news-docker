eb3e731c323ce766da58ac7a06da8521a08b517f
caroline <cluce059@gmail.com>
2021-11-16 21:06:51 -0500

    new packages

--- server/server.js
+++ server/server.js
@@ -10,7 +10,8 @@ const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  //  context: authMiddleware
 });
 
 server.applyMiddleware({ app });
@@ -23,9 +24,17 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+
 app.use(routes);
 
+ app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+ });
+
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
 
