c47bcaef8bd5d58d420abad6abf59dd097f0afd2
Conner Cochrane <cccochrane1@knights.ucf.edu>
2021-11-16 20:23:15 -0500

    switching branch

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -12,10 +12,6 @@ type User {
 
 type Circuit {
     _id: ID
-<<<<<<< HEAD
-=======
-    circuitId: String
->>>>>>> develop
     name: String
     exercises: [exercises]
 }
@@ -23,34 +19,11 @@ type Circuit {
 type Exercise{
     _id: ID!
     name: String
-<<<<<<< HEAD
     reps: 
-=======
-    reps: Int
->>>>>>> develop
     
 }
 type Auth {
     token: ID!
     user: User
 }
-<<<<<<< HEAD
 `;
-=======
-
-savedCircuits{
-    circuitId: String
-    name: String
-    exercises: [exercises]
-}
-
-type Mutation{
-    login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): Auth
-    saveCircuit(input: savedCircuit!): User
-    removeCircuit(circuitId: String!): User
-} 
-`;
-
-module.exports = typeDefs;
->>>>>>> develop
