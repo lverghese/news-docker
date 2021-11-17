17c1cb78e144375b51243276570238c8cbf27fd4
lverghese <loganverghese@gmail.com>
2021-11-16 20:50:44 -0500

    client mutations, user-controller, Article model, user-routes server side

---
+++ server/models/Article.js
@@ -0,0 +1,32 @@
+const { Schema } = require('mongoose');
+
+// This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
+const articleSchema = new Schema({
+  author: [
+    {
+      type: String,
+    },
+  ],
+  description: {
+    type: String,
+    required: true,
+  },
+  // saved article id from GoogleNewsAPI
+  articleId: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = articleSchema;
