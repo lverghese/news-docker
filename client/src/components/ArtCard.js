e042fc0cc3fac5fe1c4856342ceabe1998215eb5
Conner Cochrane <cccochrane1@knights.ucf.edu>
2021-11-20 14:22:19 -0500

    updated the styling

--- client/src/components/ArtCard.js
+++ client/src/components/ArtCard.js
@@ -5,9 +5,9 @@ const ArtCard = (props) => {
     console.log(props)
     return (
         <Card width={18}>
-            <Card.Img src={props.urlToImage}></Card.Img>
             <Card.Title>{props.title}</Card.Title>
             <Card.Text>{props.description}</Card.Text>
+            <Card.Img src={props.urlToImage}></Card.Img>
             <Card.Link href={props.url}>Details </Card.Link>
             <Card.Link onClick={(e) => props.handleSave(e)}>Save</Card.Link>
             
