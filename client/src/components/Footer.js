ac41194c5adb1ba13b61cf122ac601a9bd3197a9
Justin Bedford <jbedford94@gmail.com>
2021-11-23 21:32:39 -0500

    footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,21 +2,22 @@
 import Card from 'react-bootstrap/Card'
 import React from 'react';
 
-const navFooter = () => {
+const navFooter = () => <footer className="page-footer font-small blue pt-4">
+    <div className="container-fluid text-center text-md-left">
+        <div className="row">
+            <div className="col-md-6 mt-md-0 mt-3">
+                <h5 className="text-uppercase">Big Eel Productions</h5>
+                <p>Redifined and Filtered News</p>
+            </div>
 
-    return (
-        <Card>
-  <Card.Header></Card.Header>
-  <Card.Body>
-    <blockquote className="blockquote mb-0">
-     
-      <footer className="blockquote-footer">
-        Made with Love ( will be adjusted )
-      </footer>
-    </blockquote>
-  </Card.Body>
-</Card>
-    )
-}
+            <hr className="clearfix w-100 d-md-none pb-0"/>
+            <div className="col-md-4 mb-md-0 mb-3">
+                <h5 className="text-uppercase">Made By the Creators of Fitness Buddy</h5>
+            </div>
+        </div>
+    </div>
+
+
+</footer>
 
 export default navFooter
